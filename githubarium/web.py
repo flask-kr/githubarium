18a5209c1e63292f328965725131d9c99b8f9f5e
Eunchong Yu <kroisse@gmail.com>
2014-02-17 02:54:06 +0900

    Now runserver.py uses local configuration file

--- githubarium/web.py
+++ githubarium/web.py
@@ -1,3 +1,5 @@
+import collections
+
 from flask import Flask
 
 from .util import import_string
@@ -9,10 +11,20 @@ BLUEPRINTS = [
 
 
 class Application(Flask):
-    def initialize(self):
+    def initialize(self, config=None):
+        if config is not None:
+            self.update_config(config)
         for location, options in BLUEPRINTS:
             bp = import_string(location, 'githubarium')
             self.register_blueprint(bp, **options)
+
+    def update_config(self, config):
+        if isinstance(config, str):
+            self.config.from_pyfile(config)
+        elif isinstance(config, collections.Mapping):
+            self.config.update(config)
+        else:
+            self.config.from_object(config)
 
 app = Application(__name__)
 app.config.from_object('githubarium.default_config')
--- runserver.py
+++ runserver.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+import base64
+import os.path
 from githubarium.web import app
 
 
 if __name__ == '__main__':
-    app.initialize()
+    config_path = os.path.abspath('local.cfg')
+    if not os.path.exists(config_path):
+        print('Creating default configuration file to {}...'
+              .format(config_path))
+        with open('local.cfg', 'w', encoding='utf-8') as f:
+            secret_key = base64.b64encode(os.urandom(33))
+            print('SECRET_KEY = {!r}'.format(secret_key), file=f)
+            print("GITHUB_CONSUMER_KEY = ''  # required", file=f)
+            print("GITHUB_CONSUMER_SECRET = ''  # required", file=f)
+    app.initialize(config_path)
     app.run(debug=True)
